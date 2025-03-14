a352ab159fb46311ccb86efab90ae653441cc02d
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-03-14 19:58:51 +0500

    Get the news from the website.

--- hacker_news_headline_email.py
+++ hacker_news_headline_email.py
@@ -12,7 +12,25 @@ now = datetime.datetime.now() #current date and time
 #for email sending 
 import smtplib
 
+content = ''
 
+#func. to get Hacker News Stories
+def getNews(url):
+    print('Extracting the Hacker News Stories...')
+    cnt = ''
+    cnt += ('<b>Headline Stories : </b>' + '<br>' + '-'*50 + '<br>')
+    response = requests.get(url)
+    content = response.content
+    HTML = BeautifulSoup(content, 'html.parser')
+
+    for i, tag in enumerate(HTML.findAll('td', attrs = {'class':'title', 'valign' : ''})):
+        cnt += ((str(i+1) + ' :: ' + tag.text + "\n" +'<br>') if tag.text!='More' else '')
+        return cnt
+
+#Getting the News Content
+rawContent = getNews('https://news.ycombinator.com/')
+content += rawContent
+content += ('<br>------<br><br>End of Message!')
 
 
 
