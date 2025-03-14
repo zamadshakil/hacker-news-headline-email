02884362bfce8648b07d288dc3c2ece6c6590e10
zamadshakil <146313727+zamadshakil@users.noreply.github.com>
2025-03-14 19:57:38 +0500

    imported date and time and the otehr smtp and email mime,

--- hacker_news_headline_email.py
+++ hacker_news_headline_email.py
@@ -1,6 +1,18 @@
 import requests #for http requests
 from bs4 import BeautifulSoup #for web scraping 
 
+#for email body
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#for date and time
+import datetime
+now = datetime.datetime.now() #current date and time
+
+#for email sending 
+import smtplib
+
+
 
 
 
